135b85247258900e5a02632785848eee15a8b721
Kevin Langat <kevin.langat@nathandigital.com>
2023-12-20 11:39:33 +0300

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by kevin on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
